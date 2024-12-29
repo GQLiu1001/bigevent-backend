a1c6249c88dc8a615685a838d93f5ca79bc161f5
RabbitTank <moondust1196551872@gmail.com>
2024-12-29 16:32:14 +0800

    Update AliossUtil.java

--- src/main/java/com/example/utils/AliossUtil.java
+++ src/main/java/com/example/utils/AliossUtil.java
@@ -11,11 +11,6 @@ import java.io.FileInputStream;
 import java.io.InputStream;
 
 public class AliossUtil {
-    private static final String ENDPOINT = "https://oss-cn-shanghai.aliyuncs.com";
-    private static final String ACCESS_KEY_ID = "";
-    private static final String ACCESS_KEY_SECRET = "";
-    private static final String BUCKETNAME = "big-event-rabbittank";
-    private static final String REGION = "cn-shanghai";
     public static String uploadFile(String objectName , InputStream in) throws Exception {
 //     创建OSSClient实例。
         ClientBuilderConfiguration clientBuilderConfiguration = new ClientBuilderConfiguration();
@@ -62,18 +57,3 @@ public class AliossUtil {
                 "https://"+BUCKETNAME+"."+ENDPOINT.substring(ENDPOINT.lastIndexOf("/")+1)+"/"+objectName;
     }
 }
-// Endpoint以华东1（杭州）为例，其它Region请按实际情况填写。
-//    String endpoint = "https://oss-cn-shanghai.aliyuncs.com";
-// 从环境变量中获取访问凭证。运行本代码示例之前，请确保已设置环境变量OSS_ACCESS_KEY_ID和OSS_ACCESS_KEY_SECRET。
-//        EnvironmentVariableCredentialsProvider credentialsProvider = CredentialsProviderFactory.newEnvironmentVariableCredentialsProvider();
-//    String ACCESS_KEY_ID = "";
-//    String ACCESS_KEY_SECRET = "";
-// 填写Bucket名称，例如examplebucket。
-//    String bucketName = "big-event-rabbittank";
-// 填写Object完整路径，完整路径中不能包含Bucket名称，例如exampledir/exampleobject.txt。
-//    String objectName = "001.jpg";
-// 填写本地文件的完整路径，例如D:\\localpath\\examplefile.txt。
-// 如果未指定本地路径，则默认从示例程序所属项目对应本地路径中上传文件。
-//    String filePath = "D:\\upload\\001.jpg";
-// 填写Bucket所在地域。以华东1（杭州）为例，Region填写为cn-hangzhou。
-//    String region = "cn-shanghai";
